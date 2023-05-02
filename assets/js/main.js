7cac270b9e189021a03035cc6bba0fe33ff49fc9
0xAru <lolavilatte@gmail.com>
2023-05-02 15:37:07 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,13 @@ function getPokemon(name){
     .then(data => {
         data.json()
         .then(res => {
+            console.log(res.types);
            let cardContainer = document.getElementById("cardContainer");
            let container = document.createElement("div");
            container.classList.add("box");
            let para = document.createElement("p");
            cardContainer.appendChild(container);
-           para.innerHTML = "Nom: " + res.name;
+           para.innerHTML = "Nom: <br>" + res.name;
            container.appendChild(para);
            let img = document.createElement("img");
            img.src = res.sprites.front_default;
@@ -27,7 +28,6 @@ function getPokemon(name){
 
 
 btn = document.getElementById("submit");
-console.log(btn);
 btn.addEventListener("click", ()=>{
     element = document.getElementById("pokemon").value.toLowerCase();
     getPokemon(element);
